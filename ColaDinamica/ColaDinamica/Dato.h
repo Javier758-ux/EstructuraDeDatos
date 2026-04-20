ebbfcbfc7205c3571deb68a46c3400cb2fddeb32
Javier758-ux <a2025111797@estudiantes.upsa.edu.bo>
2026-04-20 19:12:39 -0400

    Add files via upload

---
+++ ColaDinamica/ColaDinamica/Dato.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+using namespace std;
+class Dato {
+public:
+    double Codigo;
+    string Nombre;
+    string Carrera;
+    Dato() {
+        Codigo = 0;
+        Nombre = "Unknow";
+        Carrera = "Unknow";
+    }
+};
