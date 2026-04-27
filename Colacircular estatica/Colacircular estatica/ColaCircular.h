b8c6f42b2da5ea194282094407e6629429624815
Javier758-ux <a2025111797@estudiantes.upsa.edu.bo>
2026-04-27 13:20:12 -0400

    Add files via upload

---
+++ Colacircular estatica/Colacircular estatica/ColaCircular.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+#define MAX 10
+using namespace std;
+
+class ColaCircular
+{
+    private:
+        string datos[MAX];
+        int frente;
+        int fin;
+        int cantidad;
+
+    public:
+        ColaCircular();
+
+        bool estaLlena();
+        bool estaVacia();
+        bool encolar(string valor);
+
+        string desencolar();
+        void mostrar();
+};
+
